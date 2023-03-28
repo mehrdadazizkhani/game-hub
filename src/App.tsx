d290b6209dbf38bb091a816e5916c7a12cace10d
Mehrdad Azizkhani <mehrdad.azizkhani@gmail.com>
2023-03-29 01:21:02 +0330

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import ListGroup from "./components/ListGroup";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <ListGroup />;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav""aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++
@@ -1,5 +0,0 @@
-const ListGroup = () => {
-  return <></>;
-};
-
-export default ListGroup;
