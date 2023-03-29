fc8d26cd8c02dc65a3de062bdf79a610d44d3dbc
Mehrdad Azizkhani <mehrdad.azizkhani@gmail.com>
2023-03-29 05:39:46 +0330

    Filter games by genre

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import useData from "./useData";
+import { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
@@ -13,6 +14,9 @@ export interface Game {
   metacritic: number;
 }
 
-const useGame = () => useData<Game>("/games");
+const useGame = (selectedGenre: Genre | null) =>
+  useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
+    selectedGenre?.id,
+  ]);
 
 export default useGame;
