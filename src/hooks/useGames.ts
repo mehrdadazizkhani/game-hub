15685a4ec8e3137fcb94b3c1fff33605ae8f898e
Mehrdad Azizkhani <mehrdad.azizkhani@gmail.com>
2023-04-01 06:27:43 +0330

    Implement searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,6 +23,7 @@ const useGame = (gameQuery: GameQuery) =>
         genres: gameQuery.genre?.id,
         plattforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText,
       },
     },
     [gameQuery]
